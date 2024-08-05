561056f91baa90b65e33239fb67f39cd9f41a0f2
Haimzviroz <haimzviroz@gmail.com>
2024-08-05 10:28:10 +0300

    change before pull

--- ShoesHat/Models/Shoe.cs
+++ ShoesHat/Models/Shoe.cs
@@ -1,4 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using static System.Net.Mime.MediaTypeNames;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
 
 namespace ShoesHat.Models
 {
@@ -6,10 +9,31 @@ namespace ShoesHat.Models
     {
         [Key]
         public int Id { get; set; }
-        [Display(Name = "מידה")] public int size { get; set; }
-        [Display(Name = "מותג")] public string brand { get; set; }
-        [Display(Name = "תמונה")] public byte image { get; set; }
+        [Display(Name = "מידה")] public int Size { get; set; }
+        [Display(Name = "מותג")] public string Brand { get; set; } = string.Empty;
+        [Display(Name = "תמונה")] public Byte[] Image { get; set; }
 
+        [Display(Name = "הוספת תמונה"), NotMapped]
+        public IFormFile setImage
+        {
+            get { return null; }
+            set
+            {
+                if (value == null) { return; }
+                AddImage(value);
+
+            }
+        }
+        public void AddImage(IFormFile image)
+        {
+            if (image == null) return;
+
+            using (var stream = new MemoryStream())
+            {
+                image.CopyTo(stream);
+                Image = stream.ToArray(); // Store the image data as a byte array
+            }
+        }
 
     }
 }
