e6c06ea56c0c89029b809e8101b35d1b40b682ae
harel2024 <aral236262@gmail.com>
2024-08-05 09:26:08 +0300

    עכגעכ

---
+++ ShoesHat/Models/hat.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ShoesHat.Models
+{
+    public class hat
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "צבע")] public string Color { get; set; }
+        [Display(Name = "מידה")] public int size { get; set; }
+        [Display(Name = "מותג")] public string brand { get; set; }
+        [Display(Name = "תמונה")] public byte image { get; set; }
+    
+    }
+}
