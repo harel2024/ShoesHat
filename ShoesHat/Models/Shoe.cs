9e1ea73d2911d446d9169b1659538f5beb16d37c
Haimzviroz <haimzviroz@gmail.com>
2024-08-05 11:06:23 +0300

    fix

--- ShoesHat/Models/Shoe.cs
+++ ShoesHat/Models/Shoe.cs
@@ -7,32 +7,13 @@ namespace ShoesHat.Models
 {
     public class Shoe
     {
-        [Key]
         public int Id { get; set; }
+
+        [Display(Name = "צבע")] public string Color { get; set; }
         [Display(Name = "מידה")] public int size { get; set; }
         [Display(Name = "מותג")] public string brand { get; set; }
-        [Display(Name = "צבע")] public string color { get; set; }
-        [Display(Name = "תמונה")] public Byte[] Image { get; set; }
-        public IFormFile setImage
-        {
-            get { return null; }
-            set
-            {
-                if (value == null) { return; }
-                AddImage(value);
+        [Display(Name = "תמונה")] public string image { get; set; }
 
-            }
-        }
-        public void AddImage(IFormFile image)
-        {
-            if (image == null) return;
-
-            using (var stream = new MemoryStream())
-            {
-                image.CopyTo(stream);
-                Image = stream.ToArray(); // Store the image data as a byte array
-            }
-        }
 
 
     }
